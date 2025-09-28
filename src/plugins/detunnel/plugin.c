a7908cc611b48773076ca6f705708ec929d7a992
m.nemati <nemati.mahdi255@gmail.com>
2025-09-29 02:04:02 +0330

    finish debuging  ethernet-node

---
+++ src/plugins/detunnel/plugin.c
@@ -0,0 +1,8 @@
+#include <vlib/vlib.h>
+#include <vnet/plugin/plugin.h>
+#include <vpp/app/version.h>
+
+VLIB_PLUGIN_REGISTER () = {
+  .version = VPP_BUILD_VER,
+  .description = "Generic Routing Encapsulation (GRE) plugin",
+};
