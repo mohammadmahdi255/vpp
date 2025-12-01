06e46dd67b9fd848daaf8b1ef33f03ad77be2c27
Mohammad Mahdi Nemati Haravani <nemati.mahdi255@gmail.com>
2025-12-01 20:00:38 +0330

    update ethernet detunnel

--- src/plugins/detunnel/detunnel.h
+++ src/plugins/detunnel/detunnel.h
@@ -1,8 +1,22 @@
 #include "vppinfra/types.h"
 
-// #define foreach_detunnel_protocol			\
-// 		_ (ETHERNET, ethernet)				\
-// 		_ (VLAN, vlan)
+#define foreach_ethernet_detunnel_counter		\
+	_(TOTAL, total)								\
+	_(PROCESSED, processed)						\
+	_(FAILED, failed)
+
+#define foreach_ethernet_detunnel_next_node		\
+	_(ERROR_DROP, "error-drop")					\
+	_(VLAN_DETUNNEL, "vlan-detunnel")			\
+	_(IP4_DETUNNEL, "ip4-drop")					\
+	_(IP6_DETUNNEL, "ip6-drop")
+
+
+/*
+#define foreach_detunnel_protocol			\
+		_ (ETHERNET, ethernet)				\
+		_ (VLAN, vlan)
+*/
 
 // typedef enum {
 // #define _(E, ...) DETUNNEL_##E,
@@ -11,12 +25,12 @@
 // 	DETUNNEL_STATISTICS_N
 // } detunnel_statistics_t;
 
-typedef struct
-{
-	clib_thread_index_t index;
-} detunnel_worker_t;
+// typedef struct
+// {
+// 	clib_thread_index_t index;
+// } detunnel_worker_t;
 
-typedef struct
-{
-	// vlib_combined_counter_main_t stat[DETUNNEL_STATISTICS_N];
-} detunnel_main_t;
+// typedef struct
+// {
+// 	// vlib_combined_counter_main_t stat[DETUNNEL_STATISTICS_N];
+// } detunnel_main_t;
