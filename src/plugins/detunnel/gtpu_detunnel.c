c3b3a94c2311b0d056fb88ce867fdecf99c10128
Mohammad Mahdi Nemati Haravani <nemati.mahdi255@gmail.com>
2026-02-03 17:04:10 +0330

    add gtpu

--- src/plugins/detunnel/gtpu_detunnel.c
+++ src/plugins/detunnel/gtpu_detunnel.c
@@ -10,31 +10,39 @@
 #include "vnet/ip/ip4_packet.h"
 #include "vnet/ip/ip_packet.h"
 
-#define foreach_ipv4_detunnel_next					\
+#define foreach_gtpu_detunnel_next					\
 	_(drop_next, DROP, "drop")						\
 	_(ipv4_next, IPV4_DETUNNEL, "ipv4-detunnel")	\
-	_(ipv6_next, IPV6_DETUNNEL, "ip6-drop")			\
-	_(udp_next, UDP_DETUNNEL, "udp-detunnel")
+	_(ipv6_next, IPV6_DETUNNEL, "ip6-drop")
+
+#define foreach_gtpu_protocol	\
+	_(ipv4_version)			\
+	_(ipv6_version)
 
 enum
 {
-#define _(var, id, name) IPV4_NEXT_##id,
-	foreach_ipv4_detunnel_next
+#define _(var, id, name) GTPU_NEXT_##id,
+	foreach_gtpu_detunnel_next
 #undef _
-	IPV4_NEXT_N,
+	GTPU_NEXT_N,
 };
 
 #define _(var, id, name) static SIMD_TYPE DETUNNEL_CONCAT(var, SIMD_TYPE);
 
-foreach_ipv4_detunnel_next
+foreach_gtpu_detunnel_next
+#undef _
+
+#define _(var) static SIMD_TYPE DETUNNEL_CONCAT(var, SIMD_TYPE);
+
+foreach_gtpu_protocol
 #undef _
 
 enum
 {
-#define _(id, name) IPV4_##id,
+#define _(id, name) GTPU_##id,
 	foreach_detunnel_counter
 #undef _
-	IPV4_COUNTER_N,
+	GTPU_COUNTER_N,
 };
 
 typedef struct
@@ -45,27 +53,25 @@ typedef struct
 
 typedef struct {
 	u32 counter_if_index;
-	vlib_combined_counter_main_t counters[IPV4_COUNTER_N];
+	vlib_combined_counter_main_t counters[GTPU_COUNTER_N];
 } ipv4_detunnel_main_t;
 
 extern ipv4_detunnel_main_t ipv4_detunnel_main;
 extern vlib_node_registration_t ipv4_detunnel;
 
 static_always_inline void
-ipv4_to_next(u16 *next, u16 len)
+gtpu_to_next(u16 *next, u16 len)
 {
 	for (u16 i = 0; i < len; i += SIMD_SIZE)
 	{
 		SIMD_TYPE ip_protocol_vec = SIMD_LOAD(next + i);
 		SIMD_TYPE ipv4_mask_vec = (ip_protocol_vec == SIMD_VEC(ipv4_protocol));
 		SIMD_TYPE ipv6_mask_vec = (ip_protocol_vec == SIMD_VEC(ipv6_protocol));
-		SIMD_TYPE udp_mask_vec = (ip_protocol_vec == SIMD_VEC(udp_protocol));
-		SIMD_TYPE drop_mask_vec = ~(udp_mask_vec | ipv4_mask_vec | ipv6_mask_vec);
+		SIMD_TYPE drop_mask_vec = ~(ipv4_mask_vec | ipv6_mask_vec);
 
 		SIMD_TYPE result =
 				(ipv4_mask_vec & SIMD_VEC(ipv4_next)) |
 				(ipv6_mask_vec & SIMD_VEC(ipv6_next)) |
-				(udp_mask_vec & SIMD_VEC(udp_next)) |
 				(drop_mask_vec & SIMD_VEC(drop_next));
 
 		SIMD_STORE(result, next + i);
@@ -150,21 +156,21 @@ process_buffer_4x(vlib_main_t *vm, vlib_node_runtime_t *node,
 	next[2] = ip2->protocol;
 	next[3] = ip3->protocol;
 
-	vlib_increment_combined_counter(&idm->counters[IPV4_TOTAL],
+	vlib_increment_combined_counter(&idm->counters[GTPU_TOTAL],
 		vm->thread_index, sw_idx0, 1, ip4_hdr_len0);
-	vlib_increment_combined_counter(&idm->counters[IPV4_PROCESSED],
+	vlib_increment_combined_counter(&idm->counters[GTPU_PROCESSED],
 		vm->thread_index, sw_idx0, 1, ip4_hdr_len0);
-	vlib_increment_combined_counter(&idm->counters[IPV4_TOTAL],
+	vlib_increment_combined_counter(&idm->counters[GTPU_TOTAL],
 		vm->thread_index, sw_idx1, 1, ip4_hdr_len1);
-	vlib_increment_combined_counter(&idm->counters[IPV4_PROCESSED],
+	vlib_increment_combined_counter(&idm->counters[GTPU_PROCESSED],
 		vm->thread_index, sw_idx1, 1, ip4_hdr_len1);
-	vlib_increment_combined_counter(&idm->counters[IPV4_TOTAL],
+	vlib_increment_combined_counter(&idm->counters[GTPU_TOTAL],
 		vm->thread_index, sw_idx2, 1, ip4_hdr_len2);
-	vlib_increment_combined_counter(&idm->counters[IPV4_PROCESSED],
+	vlib_increment_combined_counter(&idm->counters[GTPU_PROCESSED],
 		vm->thread_index, sw_idx2, 1, ip4_hdr_len2);
-	vlib_increment_combined_counter(&idm->counters[IPV4_TOTAL],
+	vlib_increment_combined_counter(&idm->counters[GTPU_TOTAL],
 		vm->thread_index, sw_idx3, 1, ip4_hdr_len3);
-	vlib_increment_combined_counter(&idm->counters[IPV4_PROCESSED],
+	vlib_increment_combined_counter(&idm->counters[GTPU_PROCESSED],
 		vm->thread_index, sw_idx3, 1, ip4_hdr_len3);
 
 	if (PREDICT_FALSE(node->flags & VLIB_NODE_FLAG_TRACE))
@@ -184,7 +190,7 @@ process_buffer_1x(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_buffer_t *b, 
 	ipv4_detunnel_main_t *idm = &ipv4_detunnel_main;
 	u32 sw_idx = vnet_buffer(b)->sw_if_index[VLIB_RX];
 
-	vlib_increment_combined_counter(&idm->counters[IPV4_TOTAL], vm->thread_index,
+	vlib_increment_combined_counter(&idm->counters[GTPU_TOTAL], vm->thread_index,
 			sw_idx, 1, b->current_length);
 
 	const ip4_header_t *ip4 = vlib_buffer_get_current(b);
@@ -194,15 +200,15 @@ process_buffer_1x(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_buffer_t *b, 
 
 	if (PREDICT_FALSE(ip4_hdr_len < sizeof(ip4_header_t) || ip4_pad_len < 0))
 	{
-		vlib_increment_combined_counter(&idm->counters[IPV4_FAILED], vm->thread_index,
+		vlib_increment_combined_counter(&idm->counters[GTPU_FAILED], vm->thread_index,
 				sw_idx, 1, b->current_length);
-		next[0] = IPV4_NEXT_DROP;
+		next[0] = GTPU_NEXT_DROP;
 		return;
 	}
 
 	b->current_length -= ip4_pad_len;
 	vlib_buffer_advance(b, ip4_hdr_len);
-	vlib_increment_combined_counter(&idm->counters[IPV4_PROCESSED], vm->thread_index,
+	vlib_increment_combined_counter(&idm->counters[GTPU_PROCESSED], vm->thread_index,
 			sw_idx, 1, ip4_hdr_len);
 
 	if (PREDICT_FALSE(b->flags & VLIB_BUFFER_IS_TRACED))
@@ -232,13 +238,13 @@ VLIB_NODE_FN (ipv4_detunnel) (vlib_main_t *vm, vlib_node_runtime_t *node, vlib_f
 
 	if (PREDICT_FALSE(idm->counter_if_index < max_sw_if_index))
 	{
-#define _(id, name) vlib_validate_combined_counter(&idm->counters[IPV4_##id], max_sw_if_index);
+#define _(id, name) vlib_validate_combined_counter(&idm->counters[GTPU_##id], max_sw_if_index);
 	foreach_detunnel_counter
 #undef _
 
 		for (u32 i = idm->counter_if_index + 1; i <= max_sw_if_index; i++)
 		{
-#define _(id, name) vlib_zero_combined_counter(&idm->counters[IPV4_##id], i);
+#define _(id, name) vlib_zero_combined_counter(&idm->counters[GTPU_##id], i);
 	foreach_detunnel_counter
 #undef _
 		}
@@ -284,7 +290,7 @@ VLIB_NODE_FN (ipv4_detunnel) (vlib_main_t *vm, vlib_node_runtime_t *node, vlib_f
 		n_left_from--;
 	}
 
-	ipv4_to_next(nexts, frame->n_vectors);
+	gtpu_to_next(nexts, frame->n_vectors);
 	vlib_buffer_enqueue_to_next(vm, node, from, nexts, frame->n_vectors);
 
 	return frame->n_vectors;
@@ -302,15 +308,15 @@ static u8 *format_ipv4_trace(u8 *s, va_list *args)
 			t->sw_if_index, t->ip4.protocol, t->ip4.checksum);
 }
 
-VLIB_REGISTER_NODE (ipv4_detunnel) = {
-	.name = "ipv4-detunnel",
+VLIB_REGISTER_NODE (gtpu_detunnel) = {
+	.name = "gtpu-detunnel",
 	.vector_size = sizeof(u32),
 	.format_trace = format_ipv4_trace,
 	.type = VLIB_NODE_TYPE_INTERNAL,
-	.n_next_nodes = IPV4_NEXT_N,
+	.n_next_nodes = GTPU_NEXT_N,
 	.next_nodes = {
-#define _(var, id, name) [IPV4_NEXT_##id] = (name),
-	foreach_ipv4_detunnel_next
+#define _(var, id, name) [GTPU_NEXT_##id] = (name),
+	foreach_gtpu_detunnel_next
 #undef _
 	},
 };
@@ -321,10 +327,9 @@ CLIB_MARCH_FN (ipv4_detunnel_init, clib_error_t *, vlib_main_t *CLIB_UNUSED(vm))
 {
 	clib_warning("size: %lu %s", SIMD_SIZE, CLIB_STRING_MACRO(SIMD_TYPE));
 
-	SIMD_VEC(drop_next) = SIMD_SPLAT(IPV4_NEXT_DROP);
-	SIMD_VEC(ipv4_next) = SIMD_SPLAT(IPV4_NEXT_IPV4_DETUNNEL);
-	SIMD_VEC(ipv6_next) = SIMD_SPLAT(IPV4_NEXT_IPV6_DETUNNEL);
-	SIMD_VEC(udp_next) = SIMD_SPLAT(IPV4_NEXT_UDP_DETUNNEL);
+	SIMD_VEC(drop_next) = SIMD_SPLAT(GTPU_NEXT_DROP);
+	SIMD_VEC(ipv4_next) = SIMD_SPLAT(GTPU_NEXT_IPV4_DETUNNEL);
+	SIMD_VEC(ipv6_next) = SIMD_SPLAT(GTPU_NEXT_IPV6_DETUNNEL);
 
 	return 0;
 }
@@ -337,7 +342,7 @@ static clib_error_t *ipv4_detunnel_init(vlib_main_t *CLIB_UNUSED(vm))
 	idm->counter_if_index = pool_elts(im->sw_interfaces);
 
 #define _(E, n)																\
-	vlib_combined_counter_main_t *cm_##n = &idm->counters[IPV4_##E];	\
+	vlib_combined_counter_main_t *cm_##n = &idm->counters[GTPU_##E];	\
 	cm_##n->name = "ipv4_" #n;											\
 	cm_##n->stat_segment_name = "/detunnel/ipv4/" #n;					\
 	vlib_validate_combined_counter(cm_##n, idm->counter_if_index);			\
